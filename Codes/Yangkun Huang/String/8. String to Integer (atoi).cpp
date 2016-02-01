3128c8e2ea92acc8308299f7d80fcaa3c3924d74
Burlesque <yh1875@nyu.edu>
2016-01-31 23:27:41 -0500

    Update 8. String to Integer (atoi).cpp

--- Codes/Yangkun Huang/String/8. String to Integer (atoi).cpp
+++ Codes/Yangkun Huang/String/8. String to Integer (atoi).cpp
@@ -14,11 +14,8 @@ int myAtoi(const string &str) {
 	for (; i < n; i++) {
 		if (str[i] < '0' || str[i] > '9')
 			break;
-		if (num > INT_MAX / 10 ||
-			(num == INT_MAX / 10 &&
-			(str[i] - '0') > INT_MAX % 10)) {
+		if (num > INT_MAX / 10 || (num == INT_MAX / 10 && (str[i] - '0') > INT_MAX % 10)) 
 			return sign == -1 ? INT_MIN : INT_MAX;
-		}
 		num = num * 10 + str[i] - '0';
 	}
 	return num * sign;
