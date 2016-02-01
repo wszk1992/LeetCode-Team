d39beb38bd76ea7ea223171c2e636d35d806299f
Kan Zheng <wszk1992@gmail.com>
2016-01-31 19:39:57 -0600

    modif 67

--- Codes/Kan Zheng/C++/string/67. Add Binary.cpp
+++ Codes/Kan Zheng/C++/string/67. Add Binary.cpp
@@ -9,14 +9,8 @@ public:
         
         for(i=a.length()-1,j=b.length()-1;i>=0||j>=0;i--,j--)
         {
-            if(i<0)
-                add1 = 0;
-            else
-                add1 = a[i]-'0';
-            if(j<0)
-                add2 = 0;
-            else
-                add2 = b[j]-'0';
+            add1 = i<0?0:a[i]-'0';
+            add2 = j<0?0:b[j]-'0';
             unit = add1^add2^carry+'0';
             carry = add1+add2+carry>1?1:0;
             c.insert(c.begin(),unit);
