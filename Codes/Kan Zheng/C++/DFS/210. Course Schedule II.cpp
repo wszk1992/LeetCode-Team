73f70a5599af405995fe8bf07d6a7657ed52ebed
Kan Zheng <wszk1992@gmail.com>
2016-08-04 23:13:53 +0800

    210

--- Codes/Kan Zheng/C++/DFS/210. Course Schedule II.cpp
+++ Codes/Kan Zheng/C++/DFS/210. Course Schedule II.cpp
@@ -2,30 +2,42 @@ class Solution {
 public:
     vector<int> findOrder(int numCourses, vector<pair<int, int>>& prerequisites) {
         vector<vector<int>> vertex(numCourses, vector<int>());
-        vector<int> indegree(numCourses, 0);
+        for(auto course : prerequisites) {
+            vertex[course.second].push_back(course.first);
+        }
+        vector<bool> visited(numCourses, false);
+        stack<int> s;
         vector<int> res;
-        queue<int> q;
-        for(int i = 0; i < prerequisites.size(); i++) {
-            vertex[prerequisites[i].second].push_back(prerequisites[i].first);
-            indegree[prerequisites[i].first]++;
-        }
         for(int i = 0; i < numCourses; i++) {
-            if(indegree[i] == 0) {
-                q.push(i);
-            }
-        }
-        int num = 0;
-        while(!q.empty()) {
-            vector<int> next = vertex[q.front()];
-            res.push_back(q.front());
-            q.pop();
-            for(int i = 0; i < next.size(); i++) {
-                if(--indegree[next[i]] == 0) {
-                    q.push(next[i]);
+            vector<bool> visiting(numCourses, false);
+            if(!visited[i]) {
+                if(!dfs(vertex, visiting, visited, s, i)) {
+                    return res;
                 }
             }
-            num++;
         }
-        return num == numCourses ? res : vector<int>();
+        
+        while(!s.empty()) {
+            res.push_back(s.top());
+            s.pop();
+        }
+        return res;
+    }
+    
+    bool dfs(vector<vector<int>>& vertex, vector<bool>& visiting, vector<bool>& visited, stack<int>& s, int label) {
+        if(visiting[label] && !visited[label]) {
+            return false;
+        }
+        visiting[label] = true;
+        for(auto next : vertex[label]) {
+            if(!visited[next]) {
+                if(!dfs(vertex, visiting, visited, s, next)) {
+                    return false;
+                }
+            }
+        }
+        visited[label] = true;
+        s.push(label);
+        return true;
     }
 };
